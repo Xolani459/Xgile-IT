74c6ee4ee8e41b1d60272c0b1dbcfdcbf3fbad59
Xolani459 <54267588+Xolani459@users.noreply.github.com>
2022-06-13 23:34:31 +0200

    Add files via upload

---
+++ Assignment3/Collections/CollectionsQ7/src/Numbers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Constraints
+       // int y=0;
+        int line_tot=0;
+        String q="";
+
+        line_tot= (int) Math.pow(10,5);
+        for (int z=0;z<=;z++)
+        {
+            if (y >=0 && y <=127 )
+            {
+                System.out.println("Enter the Integers :");
+                q=sc.nextLine();
+                q.split(" ");
+                z= new int[q.length()];
+                System.out.println(q);
+                System.out.println("Enter the output number located in position: ");
+
+                System.out.println();
+            }else {
+                System.err.println("Each number must fit in signed integer");
+            }
+        }
+
+
+    }
+    public int Find(int x,int y)
+    {
+
+        return x;
+    }
+}
